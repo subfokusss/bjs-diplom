06054a19e0e19d1f9a2c221c7542c762174bf8bc
Subfokus <nkoleva@mail.ru>
2023-12-30 17:03:43 +0500

    loginPage переделан

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,54 +1,25 @@
 "use strict";
 
-class UserForm {
-    constructor() {
-      this.loginFormCallback = null;
-      this.registerFormCallback = null;
-    }
-  
-    setLoginFormCallback(callback) {
-      this.loginFormCallback = callback;
-    }
-  
-    setRegisterFormCallback(callback) {
-      this.registerFormCallback = callback;
-    }
-  }
-  
-  const userForm = new UserForm();
+const userForm = new UserForm();
 
-  function loginFormAction(data) {
-    axios.post(login, data)
-      .then(response => {
-        console.log(response.data); 
-  
-        if (response.data.success) {
-          location.reload();
-        } else {
-          showError(response.data.error); 
-        }
-      })
-      .catch(error => {
-        showError(error.message);
-      });
-  }
-  
-  userForm.setLoginFormCallback(loginFormAction);
+userForm.loginFormCallback = (data) => {
+  ApiConnector.login(data, (response) => {
+    console.log(response);
+    if (response.success) {
+      location.reload();
+    } else {
+      showError(response.error);
+    }
+  });
+};
 
-  function registerFormAction(data) {
-    axios.post(register, data)
-      .then(response => {
-        console.log(response.data); 
-  
-        if (response.data.success) {
-          location.reload(); 
-        } else {
-          showError(response.data.error); 
-        }
-      })
-      .catch(error => {
-        showError(error.message);
-      });
-  }
-  
-  userForm.setRegisterFormCallback(registerFormAction);
+userForm.registerFormCallback = (data) => {
+  ApiConnector.register(data, (response) => {
+    console.log(response);
+    if (response.success) {
+      location.reload();
+    } else {
+      showError(response.error);
+    }
+  });
+};
